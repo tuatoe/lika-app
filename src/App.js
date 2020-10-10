42082526b5e20223531005051a72707aef6b4596
Tuatoe Daynuah <tuatoedaynuah@Tuatoes-iMac-2.hsd1.mn.comcast.net>
2020-10-09 19:03:38 -0500

    action dist

--- src/App.js
+++ src/App.js
@@ -2,50 +2,13 @@ import React from 'react';
 import logo from './imgs/LIKA_1cl.png';
 import './App.css';
 
-// import firebase from './firebase'
-
-// firebase.firestore().collection('customers').add({
-//   "email": "john.doe@gmail.com",
-
-// })
-
-const handler = async (event, context) => {
-
-  const redirectUrl = 'https://google.com'
-
-  return {
-    statusCode: 302,
-    headers: {
-      Location: redirectUrl,
-      'Cache-Control': 'no-cache',
-    },
-    body: JSON.stringify({})
-  }
-}
 
 function App() {
 
-  // const db = firebase.firestore();
-
-  // const usersRef = db.collection('customers');
-
-  // const [email, setEmail] = useState('');
-
-  // const handleSubmit = (e) => {
-
-  //   e.preventDefault()
-
-  //   usersRef.add({
-  //     "email": email
-  //   })
-  //     .then(() => {
-  //       setEmail('')
-  //     })
-  // }
   return (
     <div className="App">
       <div className="wrapper">
-        <form name="customers" action="POST" method="POST" data-netlify="true" className="sign-up">
+        <form name="customers" action="POST" data-netlify="true" className="sign-up">
           <img className="logo" src={logo} alt="lika app logo" />
           <h1>SOMETHING AWESOME IS COMING</h1>
           <p>It's going to be amazing! Sign up to find our when it's ready.</p>
