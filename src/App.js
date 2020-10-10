efd85b6602f1de2bfaa6fa0ed8ca3c7ed8821e9f
Tuatoe Daynuah <tuatoedaynuah@Tuatoes-iMac-2.hsd1.mn.comcast.net>
2020-10-09 19:16:53 -0500

    prevent spam

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
   return (
     <div className="App">
       <div className="wrapper">
-        <form name="customers" action="POST" netlify data-netlify="true" className="sign-up">
+        <form name="customers" action="POST" netlify data-netlify="true" data-netlify-recaptcha="true" className="sign-up">
           <img className="logo" src={logo} alt="lika app logo" />
           <h1>SOMETHING AWESOME IS COMING</h1>
           <p>It's going to be amazing! Sign up to find our when it's ready.</p>
@@ -18,6 +18,7 @@ function App() {
                 <span className="sr-only">Email address, is required</span>
                 <input id="email" name="email" placeholder="Email address" className="form-control" type="email" required />
               </label>
+              <div data-netlify-recaptcha="true"></div>
               <button type="submit" className="btn btn-danger sign-up__button">Sign up</button>
             </div>
           </div>
